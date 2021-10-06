66e7063a9d0b023c1ce9a7723432723dec50e9a7
NikitaLyapustin <83009252+NikitaLyapustin@users.noreply.github.com>
2021-10-06 19:29:50 +0300

    [add] screen with chats

---
+++ Figma/Extensions/UILabel+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  String+Extensions.swift
+//  Figma
+//
+//  Created by Никита Ляпустин on 03.10.2021.
+//
+
+import UIKit
+
+extension UILabel {
+    func addCharacterSpacing(kernValue: Double = 1.25) {
+        if let labelText = text, labelText.count > 0 {
+            let attributedString = NSMutableAttributedString(string: labelText)
+            attributedString.addAttribute(NSAttributedString.Key.kern, value: kernValue, range: NSRange(location: 0, length: attributedString.length - 1))
+        attributedText = attributedString
+        }
+    }
+}
